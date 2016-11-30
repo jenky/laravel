680ae8a500400a3cc58d3671625471747a057a48
Lynh Tran <jenky.w0w@gmail.com>
2016-11-30 09:10:01 +0700

    Working

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Transformers\TestTransformer;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function validation(Request $request)
+    {
+        // dd($request);
+        $this->validate($request, [
+            'test' => 'required',
+        ]);
+    }
+
+    public function model()
+    {
+        return \App\User::findOrFail(100);
+    }
+
+    public function transformer()
+    {
+        return response()->transform(\App\User::paginate(), new TestTransformer);
+    }
+
+    public function exception()
+    {
+        throw new \App\Exceptions\TestException("Error Processing Request");
+    }
+}
