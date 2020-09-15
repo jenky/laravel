1e67aabe92854d3786a0845c2d4ece5bc419d59e
Lynh <jenky.w0w@gmail.com>
2020-09-15 17:37:40 +0700

    Laravel 8

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,6 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Jenky\LaravelAPI\Exception\ExceptionResponse;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
