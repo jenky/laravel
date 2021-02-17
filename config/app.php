b82a74b61843122ef1e1d69e9fe84b58347544ae
Lynh <jenky.w0w@gmail.com>
2021-02-17 18:39:24 +0700

    WIP

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Jenky\Cartolic\CartolicApplicationServiceProvider::class,
 
         /*
          * Application Service Providers...
