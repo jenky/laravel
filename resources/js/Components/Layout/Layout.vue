cb65a45f6edc5d6971d464710445c3c4dabe295d
Lynh <jenky.w0w@gmail.com>
2022-05-13 10:02:51 +0700

    Layout setup

---
+++ resources/js/Components/Layout/Layout.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+<div class="text-center mx-auto">
+  <slot />
+</div>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,16 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { ZiggyVue } from '@/../vendor/tightenco/ziggy/src/js/vue'
 import { Ziggy } from './ziggy'
+import Layout from './Components/Layout/Layout.vue'
 
 createInertiaApp({
   resolve: async (name) => {
     const pages = import.meta.glob('./Pages/**/*.vue')
+    const page = (await pages[`./Pages/${name}.vue`]()).default
+    page.layout = page.layout || Layout
 
-    return (await pages[`./Pages/${name}.vue`]()).default;
+    return page
+
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
