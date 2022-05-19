aaf8757192f4f70b927c3af3ea39678c557bfc63
Lynh <jenky.w0w@gmail.com>
2022-05-19 13:56:52 +0700

    Remove

--- resources/js/pages/About.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import { Head, Link } from '@inertiajs/inertia-vue3'
-</script>
-
-<template>
-  <Head title="About" />
-  <h3>This is about page</h3>
-  <Link href="/">Home page</Link>
-</template>
