e7328d7b900275c22a2636cc9ce5ed47c25394a2
Lynh <jenky.w0w@gmail.com>
2022-05-19 11:16:33 +0700

    Revert default layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,16 +4,12 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { ZiggyVue } from '@/../vendor/tightenco/ziggy/src/js/vue'
 import { Ziggy } from './ziggy'
-import Layout from './Components/Layout/Layout.vue'
 
 createInertiaApp({
   title: title => `${title} - ${import.meta.env.VITE_APP_NAME}`,
   resolve: async (name) => {
     const pages = import.meta.glob('./Pages/**/*.vue')
-    const page = (await pages[`./Pages/${name}.vue`]()).default
-    page.layout = page.layout || Layout
-
-    return page
+    return (await pages[`./Pages/${name}.vue`]()).default
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
