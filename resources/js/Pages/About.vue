865f374022ee8a6c74d7999d90b6ce114a8ff4e6
Lynh <jenky.w0w@gmail.com>
2022-05-19 13:57:08 +0700

    Revert remove

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3'
+</script>
+
+<template>
+  <Head title="About" />
+  <h3>This is about page</h3>
+  <Link href="/">Home page</Link>
+</template>
