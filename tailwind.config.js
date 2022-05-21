353c0bf107722ed14fd24103a410987ec1ace63d
Lynh <jenky.w0w@gmail.com>
2022-05-21 23:56:10 +0700

    Disable tw forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   plugins: [
     // require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
+    // require('@tailwindcss/forms'),
     require('daisyui'),
   ],
 }
