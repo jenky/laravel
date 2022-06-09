182561b101aa4b1cefe1083957c56e8e1adfdcbe
Lynh <jenky.w0w@gmail.com>
2022-06-09 10:18:09 +0700

    Ignore ziggy generated routes

--- resources/js/ziggy.js
+++
@@ -1,7 +0,0 @@
-const Ziggy = {"url":"http:\/\/laravel.test","port":null,"defaults":{},"routes":{"ignition.healthCheck":{"uri":"_ignition\/health-check","methods":["GET","HEAD"]},"ignition.executeSolution":{"uri":"_ignition\/execute-solution","methods":["POST"]},"ignition.updateConfig":{"uri":"_ignition\/update-config","methods":["POST"]},"index":{"uri":"\/","methods":["GET","HEAD"]},"about":{"uri":"about","methods":["GET","HEAD"]},"contact":{"uri":"contact","methods":["POST"]}}};
-
-if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-    Object.assign(Ziggy.routes, window.Ziggy.routes);
-}
-
-export { Ziggy };
